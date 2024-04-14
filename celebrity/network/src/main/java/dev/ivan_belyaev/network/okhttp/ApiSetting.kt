6b41356852c6e507e27b64c0d6a91b196b70c656
Ivan Balyaev <bunin.vania2015@yandex.ru>
2024-04-14 17:07:54 +0500

    все готово, остается почистить + токен убрал

--- celebrity/network/src/main/java/dev/ivan_belyaev/network/okhttp/ApiSetting.kt
+++ celebrity/network/src/main/java/dev/ivan_belyaev/network/okhttp/ApiSetting.kt
@@ -3,5 +3,7 @@ package dev.ivan_belyaev.network.okhttp
 object ApiSetting {
 
     const val header = "X-API-KEY"
-    const val token = "WF76VQQ-HQB4P5G-JFJH8DF-CRKDP1M"
+
+    //YOUR TOKEN
+    const val token = ""
 }
